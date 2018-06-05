338b7548d2eb0b9f2aadf335076c74464cf39533
杨扬 <jianhuyy@hotmail.com>
2018-06-05 09:00:05 +0800

    解决编码问题

--- LibImage/image_tools.h
+++ LibImage/image_tools.h
@@ -1,5 +1,8 @@
 #pragma once
 
+
+#include <algorithm>
+
 #include "image.h"
 
 
@@ -7,17 +10,17 @@
 namespace image
 {
 	
-	/*****************************   尺寸有关   *****************************/
+	/*****************************   形状尺寸有关   *****************************/
 
 	/*
-		两张图片水平合并
+		两张图片水平拼接
 	*/
 	template<typename T>
 	typename Image<T>::Ptr
 		hstack(typename Image<T>::ConstPtr img1,typename Image<T>::ConstPtr img2);
 
 	/*
-		两张图片垂直合并
+		两张图片垂直拼接
 	*/
 	template<typename T>
 	typename Image<T>::Ptr
@@ -25,9 +28,20 @@ namespace image
 
 
 
+	/*****************************   颜色有关   *****************************/
 
+	enum cvtType
+	{
+		CVT_TYPE_MAXIMUM,
+		CVT_TYPE_LIGHTNESS,
+		CVT_TYPE_LUMINOSITY,
+		CVT_TYPE_LUMINANCE,
+		CVT_TYPE_AVERAGE
+	};
 
-	
+	template<typename T>
+	typename Image<T>::Ptr
+		RGB2Gray(typename Image<T>::ConstPtr img,cvtType type);
 
 
 
@@ -36,7 +50,7 @@ namespace image
 		hstack(typename Image<T>::ConstPtr img1, typename Image<T>::ConstPtr img2)
 	{
 		if (img1->height() != img2->height() || img1->channels() != img2->channels())
-			throw std::invalid_argument("两幅图片无法水平合并");
+			throw std::invalid_argument("两幅图片无法水平拼接");
 
 		Image<T>::Ptr ret = Image<T>::create(img1->width() + img2->width(),
 			img1->height(),
@@ -59,7 +73,7 @@ namespace image
 		vstack(typename Image<T>::ConstPtr img1, typename Image<T>::ConstPtr img2)
 	{
 		if (img1->width() != img2->width() || img1->channels() != img2->channels())
-			throw std::invalid_argument("两幅图片无法垂直合并");
+			throw std::invalid_argument("两幅图片无法垂直拼接");
 
 		Image<T>::Ptr ret = Image<T>::create(img1->width(),
 			img1->height() + img2->height(),
@@ -67,6 +81,58 @@ namespace image
 		std::copy(img1->begin(), img1->end(), ret->begin());
 		std::copy(img2->begin(), img2->end(), ret->begin() + img1->GetValueAmount());
 		return ret;
+	}
+
+
+	template<typename T>
+	inline T
+		RGB2Gray_maximum(T const* v)
+	{
+		return *std::max_element(v, v + 3);
+	}
+
+	template<typename T>
+	inline T
+		RGB2Gray_lightness(T const* v)
+	{
+		T const* max = std::max_element(v, v + 3);
+		T const* min = std::min_element(v, v + 3);
+
+	}
+
+	template<typename T>
+	typename Image<T>::Ptr 
+		RGB2Gray(typename Image<T>::ConstPtr img, cvtType type)
+	{
+		if (img == nullptr)
+			throw std::invalid_argument("Null image given");
+
+		int channels = img->channels();
+		if (channels != 3 && channels != 4)
+			throw std::invalid_argument("Image must be RGB or RGBA");
+
+		bool has_alpha = (channels == 4);
+
+		typename Image<T>::Ptr out(Image<T>::Create());
+		out->allocate(img->widht(), img->height(), 1 + has_alpha);
+
+		typedef T(*RGB2GrayFunc)(T const*);
+		RGB2GrayFunc func;
+		switch (type)
+		{
+		case image::CVT_TYPE_MAXIMUM:
+			break;
+		case image::CVT_TYPE_LIGHTNESS:
+			break;
+		case image::CVT_TYPE_LUMINOSITY:
+			break;
+		case image::CVT_TYPE_LUMINANCE:
+			break;
+		case image::CVT_TYPE_AVERAGE:
+			break;
+		default:
+			break;
+		}
 	}
 
 }
